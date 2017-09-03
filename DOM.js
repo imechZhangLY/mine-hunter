29fd2aef6906852b89814956f57385cbbbe4c84e
imechZhangLY <zhangly1016@foxmail.com>
2017-09-03 20:27:52 +0800

    mine-hunter

---
+++ DOM.js
@@ -0,0 +1,98 @@
+(function dom(){
+
+	var body=document.getElementById('body')
+	var arrbutton=[]
+	var n=9
+	var mineNumber = 10
+	var urlMine = 'url(image/mine.png)'
+	var urlFlag = 'url(image/flag3.png)'
+
+	function build(){
+		var temp = []
+		
+		//屏蔽鼠标右击
+		document.getElementById('body').oncontextmenu = function(event){event.returnValue = false}
+		//生成雷
+		var arr = RandomNumber(n,mineNumber)
+		for(var i=0,element;i<9;i++){
+			for(var j=0;j<9;j++){
+				element=document.createElement('button')
+				element.setAttribute('class','button mine')
+				element.setAttribute('disabled',true)
+				element.dataset.number = arr[i][j]
+				element.dataset.id = j + i * n
+				body.appendChild(element)
+				temp.push(element)
+			}
+			arrbutton.push(temp)
+			temp = []
+			body.appendChild(document.createElement('br'))
+		};
+		var clearfix = document.createElement('div')
+		clearfix.style.clear = 'both'
+		body.appendChild(clearfix)
+		delete(element)
+		body.style.width=n*50+2*(n+9)+'px'
+	}
+
+	build()//生成页面
+
+	var inputNumberMines = document.getElementById('numberofmines')
+	var inputTime = document.getElementById('time')
+	inputTime.value = '0 s'
+	inputNumberMines.value = mineNumber
+	//开始按钮添加响应函数
+	var intervalId
+	function start(){
+
+		var t = 1
+		intervalId = setInterval(function(){
+			inputTime.value = t + ' s'
+			t = t + 1
+		},1000)
+		var i = 0
+		var j = 0
+		arrbutton.forEach(function(ele1){
+			j=0
+			ele1.forEach(function(ele2){
+				ele2.disabled = false
+				ele2.addEventListener('mousedown',FuncOnClick.bind(ele2,urlMine,urlFlag,arrbutton,i,j,n,intervalId))
+				j++
+			})
+			i++
+		})
+
+		document.getElementById('img').style.backgroundImage = "url('image/smile.svg')"
+
+
+		this.disabled = true
+	}
+	document.getElementById('start').addEventListener('click',start)
+
+	function reset(){
+		var arr = RandomNumber(n,mineNumber)
+		document.getElementById('start').disabled = false
+		clearInterval(intervalId)
+		inputTime.value = '0 s'
+		inputNumberMines.value = mineNumber
+		var i = 0
+		var j
+		arrbutton.forEach(function(ele1){
+			j = 0
+			ele1.forEach(function(ele2){
+				ele2.style = {}
+				ele2.innerText = ''
+				ele2.setAttribute('class','button mine')
+				ele2.setAttribute('disabled',true)
+				ele2.dataset.number = arr[i][j]
+				ele2.dataset.id = j + i * n
+				j++
+			})
+			i++
+		})
+	}
+	document.getElementById('reset').addEventListener('click',reset)
+	document.getElementById('img').style.backgroundImage = 'url("image/smile.svg")'
+
+
+})()
---
+++ functionlibrary.js
@@ -0,0 +1,142 @@
+function FuncOnClick(urlMine,urlNormal,arrButton,index1,index2,n,intervalId,event){
+	var flag = this.dataset.number
+	var over = false
+	if(event.which === 1){
+		if(flag==888){
+			this.style.backgroundImage=urlMine
+			gameOver(intervalId,arrButton,urlMine)
+			over = true
+
+		}else if(flag == 0){
+			this.style.backgroundColor='white';
+			this.disabled = 'disabled';
+			response(index1,index2,arrButton,n)
+		}else{
+			this.style.backgroundColor='white';
+			this.innerText='' + flag;
+			this.disabled = 'disabled';
+		}
+	}else if(event.which == 3){
+		this.style.backgroundImage=urlNormal
+		this.disabled = 'disabled'
+		if(document.getElementById('numberofmines').value - 0 > 1){
+			document.getElementById('numberofmines').value -= 1
+		}else{
+			document.getElementById('numberofmines').value -= 1
+			gameOver(intervalId,arrButton,urlMine)
+			over = true
+		}
+	}
+	if(!over){
+		success(intervalId,arrButton,urlMine)
+	}
+
+}
+function RandomNumber(n,MineNumber){
+	var arr=new Array()
+	for(var i=0;i<n;i++){
+		arr[i]=new Array();
+		for(var j=0;j<n;j++){
+			arr[i][j]=0;
+		}
+	}
+	var total = 0
+	while(total < MineNumber){
+		var index1 = Math.floor(n*Math.random())
+		var index2 = Math.floor(n*Math.random())
+		if(arr[index1][index2] !== 888){
+			arr[index1][index2] = 888
+			total = total + 1
+		}
+	}
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			if(arr[i][j]==888){
+				continue;
+			}else{
+				for(var k = -1; k < 2; k++){
+					for(var l = -1; l < 2; l++){
+						index1 = i + k
+						index2 = j + l
+						if(index1 > -1 && index1 < n && index2 > -1 && index2 < n && arr[index1][index2] === 888)
+							arr[i][j] += 1
+					}
+				}
+			}
+		}
+	}
+
+	return arr;
+}
+
+function response(id1,id2,arrButton,n){
+	var i,j,index1,index2
+	for(i = -1; i < 2; i++){
+		index1 = id1 + i
+		for(j = -1; j < 2; j++){
+			index2 = id2 + j
+			if(index1 > -1 && index1 < n && index2 > -1 && index2 < n && arrButton[index1][index2].dataset.number != 888 && !arrButton[index1][index2].disabled){
+				console.log(arrButton[index1][index2].dataset.number)
+				if(arrButton[index1][index2].dataset.number == 0){
+					arrButton[index1][index2].style.backgroundColor = 'white'
+					arrButton[index1][index2].disabled = true
+					response(index1,index2,arrButton,n)
+					console.log(index1)
+				}else{
+					arrButton[index1][index2].style.backgroundColor='white'
+					arrButton[index1][index2].innerText='' + arrButton[index1][index2].dataset.number
+					arrButton[index1][index2].disabled = true
+				}
+		}
+		}
+	}
+}
+
+function gameOver(intervalId,arrButton,urlMine){
+	allShow(intervalId,arrButton,urlMine)
+	document.getElementById('img').style.backgroundImage = 'url("image/cry.png")'
+}
+
+function success(intervalId,arrButton,urlMine){
+	var successed = true
+	arrButton.forEach(function(ele1){
+		ele1.forEach(function(ele2){
+			if(ele2.dataset.number == 888 && !ele2.disabled){
+				successed = false
+			}
+		})
+	})
+
+	if(!successed){
+		arrButton.forEach(function(ele1){
+			ele1.forEach(function(ele2){
+				if(ele2.dataset.number != 888 && !ele2.disabled){
+				successed = false
+				}
+			})
+		})
+	}
+	if(successed){
+		allShow(intervalId,arrButton,urlMine)
+		alert('恭喜你顺利通过游戏！')
+	}
+}
+
+function allShow(intervalId,arrButton,urlMine){
+	arrButton.forEach(function(element1){
+		element1.forEach(function(element2){
+			if(element2.dataset.number == 888){
+				element2.style.backgroundImage=urlMine
+			}else if(element2.dataset.number == 0){
+				element2.style.backgroundImage=''
+				element2.style.backgroundColor = 'white'
+			}else{
+				element2.style.backgroundImage=''
+				element2.style.backgroundColor = 'white'
+				element2.innerText = element2.dataset.number
+			}
+			element2.disabled = true
+		})
+	})
+	clearInterval(intervalId)
+}
